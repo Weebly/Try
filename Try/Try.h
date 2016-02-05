e15aa6a4781ed27e7a7242a18ddc40cac7d72a14
ketzusaka <james@weebly.com>
2016-02-05 10:38:10 -0800

    Initial commit

---
+++ Try/Try.h
@@ -0,0 +1,19 @@
+//
+//  Try.h
+//  Try
+//
+//  Created by James Richard on 2/5/16.
+//  Copyright © 2016 Weebly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Try.
+FOUNDATION_EXPORT double TryVersionNumber;
+
+//! Project version string for Try.
+FOUNDATION_EXPORT const unsigned char TryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Try/PublicHeader.h>
+
+#import <Try/WBTry.h>
